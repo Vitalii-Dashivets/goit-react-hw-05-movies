a49921db4fe96f5bbea40bf2638c372c7cfc5899
Vitalii-Dashivets <vdashivets1985@gmail.com>
2023-08-04 21:21:43 +0300

    com1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
+import { Routes, Route } from 'react-router-dom';
+import { Home } from '../pages/Home';
+import { Movies } from '../pages/Movies';
+import { MovieDetails } from '../pages/MovieDetails';
+import { NotFound } from '../pages/NotFound';
+import { Cast } from './Cast/Cast';
+import { Reviews } from './Reviews/Reviews';
+import { Container, Header, Link } from './App.styled';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Header>
+        <nav>
+          <Link to="/"> Home</Link>
+          <Link to="/movies">Movies</Link>
+        </nav>
+      </Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Container>
   );
 };
---
+++ src/components/App.styled.js
@@ -0,0 +1,36 @@
+import styled from '@emotion/styled';
+import { NavLink } from 'react-router-dom';
+
+export const Container = styled.div`
+  max-width: 960px;
+  margin: 10px auto;
+  padding: 0 16px;
+`;
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 12px;
+  padding: 8px 0;
+  margin-bottom: 16px;
+  border-bottom: 1px solid black;
+
+  > nav {
+    display: flex;
+    margin: 0 auto;
+  }
+`;
+
+export const Link = styled(NavLink)`
+  padding: 8px 16px;
+  border-radius: 4px;
+  text-decoration: none;
+  color: black;
+  font-weight: 500;
+
+  &.active {
+    color: white;
+    background-color: orangered;
+  }
+`;
